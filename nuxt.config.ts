e09981596a13fc23ef305c41bb7f00134e9b2f0d
greeng00se <thegreengoose7@gmail.com>
2022-09-17 21:07:11 +0900

    [#1] feat: pinia 설치 및 적용

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  ssr: false,
+  modules: [
+    '@pinia/nuxt',
+  ],
 })
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <input type="button" @click="store.increment()" value="increment">
+    {{ store.count }}
+    {{ store.doubleCount }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useCounterStore } from "~~/store/counter";
+
+const store = useCounterStore();
+</script>

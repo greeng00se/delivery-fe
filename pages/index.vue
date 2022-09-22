f1418a3ac4da79c9a395e7d146895d40da6f092c
greeng00se <thegreengoose7@gmail.com>
2022-09-22 23:29:56 +0900

    [#4] feat: 로그인 페이지 추가

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,5 @@
 <template>
   <div class="d-flex align-center flex-column">
-    {{ store.count }}
-    {{ store.doubleCount }}
-    <v-icon large color="green darken-2">mdi-domain</v-icon>
-    <v-btn color="primary" elevation="2" @click="store.increment()" outlined rounded>Click</v-btn>
+    오늘은 무엇을 먹을까요?
   </div>
 </template>
-
-<script setup lang="ts">
-import { useCounterStore } from "~~/store/counter";
-
-const store = useCounterStore();
-</script>
